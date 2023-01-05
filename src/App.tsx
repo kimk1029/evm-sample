1035ce6d25cad6f4b883e6168940614e321864ab
kimk1029ghp_XD8HMwBh9p4x8lGEh92MvyqdVcWGaC0NlQb <kimk1029@naver.com>
2023-01-05 16:38:58 +0900

    주소 가져오기 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Web3 from "web3";
+import { BlockHeader, Block } from "web3-eth";
 
 function App() {
+  const [address, setAddress] = useState<string>("");
+  const [pk, setPk] = useState<string>("");
+  const web3 = new Web3(Web3.givenProvider);
+  const clickBtn = () => {
+    const account = web3.eth.accounts.create();
+    setAddress(account.address);
+    setPk(account.privateKey);
+    console.log(Web3.givenProvider);
+  };
+  const clickReset = () => {
+    setAddress("");
+    setPk("");
+  };
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {address && <p>Address : {address}</p>}
+        {pk && <p>Key : {pk}</p>}
+        <button onClick={clickBtn}>generate</button>
+        <button onClick={clickReset}>reset</button>
       </header>
     </div>
   );
