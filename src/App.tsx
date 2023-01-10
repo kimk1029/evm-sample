0d1949d89636fe67b5fb9664be2bdd2299cb95f8
kimk1029ghp_XD8HMwBh9p4x8lGEh92MvyqdVcWGaC0NlQb <kimk1029@naver.com>
2023-01-10 14:46:39 +0900

    infura provider & pk import to address

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,56 @@
 import React, { useState } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import Web3 from "web3";
-import { BlockHeader, Block } from "web3-eth";
 
+const INFURA_URL =
+  "https://goerli.infura.io/v3/e839e4099dba4eb189fab5aeade0dfea";
 function App() {
   const [address, setAddress] = useState<string>("");
   const [pk, setPk] = useState<string>("");
-  const web3 = new Web3(Web3.givenProvider);
+
+  const web3 = new Web3(INFURA_URL);
+
   const clickBtn = () => {
     const account = web3.eth.accounts.create();
     setAddress(account.address);
     setPk(account.privateKey);
-    console.log(Web3.givenProvider);
   };
   const clickReset = () => {
     setAddress("");
     setPk("");
   };
+  const clickPktoAccount = () => {
+
+    const { address, privateKey } = web3.eth.accounts.privateKeyToAccount(
+      pk.substring(2)
+    );
+    if (address) {
+      setAddress(address);
+    }
+  };
+  const inputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = e.target;
+    setPk(value);
+  };
+  const ClickBalanceOf = () => {
+    const contract = web3
+  };
   return (
     <div className="App">
       <header className="App-header">
         {address && <p>Address : {address}</p>}
-        {pk && <p>Key : {pk}</p>}
+          {pk && <p>Key : {pk}</p>}
         <button onClick={clickBtn}>generate</button>
         <button onClick={clickReset}>reset</button>
+        <hr />
+        <input
+          name={"pk"}
+          type={"text"}
+          placeholder={"Private Key"}
+          onChange={inputChange}
+        />
+        <button onClick={clickPktoAccount}>Import Privatekey </button>
+        {address && <button onClick={ClickBalanceOf}>Balance ? </button>}
       </header>
     </div>
   );
